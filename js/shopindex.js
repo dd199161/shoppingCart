a44eb268d51fb4ab129b82b58fbfaa1b668a5d13
song chengzhuang <1109726547@qq.com>
2017-03-30 11:17:07 +0800

    bootstrap+vue.js
    纯前端。
    实现了 购物车的选择商品，删除商品，全选（取消全选），价格的双向绑定和总价的实时计算。

---
+++ js/shopindex.js
@@ -0,0 +1,80 @@
+/**
+ * Created by Administrator on 2017/3/28.
+ * 购物车首页的vue.js用于数据绑定
+ */
+new Vue({
+    el: "#notice_index",
+    data: {
+        total_price: 0,
+        noticeList: [],
+        delitem: ''
+    },
+    filters: {
+        formatMoney: function (value) {
+            return "￥" + value.toFixed(2);
+        }
+    },
+    mounted: function () {
+        this.$nextTick(function () {
+            this.noticedetail();
+        })
+    },
+    methods: {
+        //获取json文件的 数据
+        noticedetail: function () {
+            this.$http.get("json/noticeList.json").then(res => {
+                this.noticeList = res.body.noticelist;
+            })
+        },
+        //商品数量加减函数
+        changeMoney: function (notice, way) {
+            if (way > 0) {
+                notice.number++;
+                if (notice.number > 99) {
+                    notice.number = 99;
+                }
+            } else {
+                notice.number--;
+                if (notice.number < 0) {
+                    notice.number = 0;
+                }
+            }
+            this.totalPrice;
+        },
+        //商品选择
+        selectnotice: function (item, way) {
+            if (way > 0) {
+                item.checked = true;
+            } else {
+                item.checked = false;
+            }
+            this.totalPrice;
+        },
+        //商品是否要 全选中
+        selectall: function (way) {
+            this.noticeList.forEach(item => {
+                if (way > 0) {
+                    item.checked = true;
+                } else {
+                    item.checked = false;
+                }
+            })
+            this.totalPrice;
+        },
+        //删除商品
+        delconfirm: function (item) {
+            this.delitem = item;
+        }
+    },
+    computed: {
+        //结账 总金额的实时计算
+        totalPrice: function () {
+            this.total_price = 0;
+            this.noticeList.forEach(item => {
+                if (item.checked == true) {
+                    this.total_price += item.danprice * item.number;
+                }
+            })
+        }
+    }
+});
